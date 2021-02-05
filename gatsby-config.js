443da56c8ad8efdefed0408b70fa06b678e07244
Omer Gulen <omergulen@jotform.com>
2021-02-05 22:41:18 +0300

    🔥 Google Analytics added

--- gatsby-config.js
+++ gatsby-config.js
@@ -65,6 +65,12 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: `UA-122322547-6`,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // 'gatsby-plugin-offline',
