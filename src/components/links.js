22828c091643c1a06a269bba612c5b801aadd18e
Omer Gulen <omergulen@jotform.com>
2021-02-05 17:23:28 +0300

    Links :: ✨ init

---
+++ src/components/links.js
@@ -0,0 +1,95 @@
+import React, { useState } from "react"
+
+import Container from './Container';
+import NameHeader from './NameHeader';
+import OuterContainer from './OuterContainer';
+import PinButton from './PinButton';
+import PinInput from './PinInput';
+
+import '../pin.css';
+
+const Links = () => {
+  const [pinValue, setPinValue] = useState('');
+  const [pinTitle, setPinTitle] = useState('');
+  const [pinnedValues, setPinnedValues] = useState([]);
+
+  const handlePinValueChange = (event) => {
+    setPinValue(event.target.value);
+  };
+
+  const handlePinTitleValueChange = (event) => {
+    setPinTitle(event.target.value);
+  };
+
+  const handleAddClicked = () => {
+    if (pinTitle || pinValue) {
+      setPinnedValues([
+        {title: pinTitle, url: pinValue},
+        ...pinnedValues,
+      ]);
+      setPinValue('');
+      setPinTitle('');
+    }
+  };
+  const handleDeleteClicked = (index) => {
+    const array = [...pinnedValues]; // make a separate copy of the array
+    array.splice(index, 1);
+    setPinnedValues(array);
+  };
+
+  const pinCode = '123456';
+
+  return (
+    <OuterContainer>
+      <Container>
+        {pinnedValues && (
+          <ul className={'pinned-values'}>
+          {pinnedValues.map(({title, url}, index) => {
+            return (
+              <li key={index}>
+                <a target="_blank" rel="noreferrer" href={url}>{title ? title : url}</a>
+                <span className="delete-button" onClick={() => handleDeleteClicked(index)}>X</span>
+              </li>
+            )
+          })}
+        </ul>
+        )}
+        <NameHeader>
+          {pinCode}
+        </NameHeader>
+        <PinInput
+          name="pin-description"
+          type="tel"
+          placeholder="Title"
+          id="description-input"
+          className="sc-dlfnbm lflLbn"
+          autocomplete="off"
+          value={pinTitle}
+          onChange={handlePinTitleValueChange}
+          aria-expanded="false"
+          autoFocus
+        />
+        <PinInput
+          name="pin"
+          type="tel"
+          placeholder="https://example.com"
+          id="pin-input"
+          className="sc-dlfnbm lflLbn"
+          autocomplete="off"
+          value={pinValue}
+          onChange={handlePinValueChange}
+          aria-expanded="false"
+          autoFocus
+        />
+        <PinButton
+          className="enter-button__EnterButton-sc-1o9b9va-0 kfzgPK"
+          onClick={handleAddClicked}
+        >
+          Add
+        </PinButton>
+      </Container>
+    </OuterContainer>
+  );
+}
+
+export default Links;
