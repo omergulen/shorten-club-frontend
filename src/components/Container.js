6f603e597489a7c31692b3ea22861042d23aa61f
Omer Gulen <omergulen@jotform.com>
2021-02-05 17:18:58 +0300

    Container Components :: init

---
+++ src/components/Container.js
@@ -0,0 +1,7 @@
+import styled from "@emotion/styled";
+
+const Container = styled.div`
+  text-align: center;
+`;
+
+export default Container;
---
+++ src/components/OuterContainer.js
@@ -0,0 +1,11 @@
+import styled from "@emotion/styled";
+
+const OuterContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: row;
+  height: 78vh;
+`;
+
+export default OuterContainer;
