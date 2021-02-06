0c7b5b2513bbd867fd955a702401d83488093e6b
Omer Gulen <omergulen@jotform.com>
2021-02-06 15:13:15 +0300

    Layout :: footer update

--- src/components/layout.js
+++ src/components/layout.js
@@ -69,10 +69,8 @@ const Layout = ({ children }) => {
             </main>
             <Footer>
               <p>
-              © {new Date().getFullYear()}, Built with
-              {` `}
+              © {new Date().getFullYear()}, {`Built with <3`}
               </p>
-              <GatsbyLink href="https://www.gatsbyjs.org">Gatsby</GatsbyLink>
             </Footer>
           </Content>
         </>
