d544e0ae16ac7bc864d9d0b0367e13521645437c
Omer Gulen <omergulen@jotform.com>
2021-02-08 23:17:16 +0300

    404 :: test

--- src/pages/404.js
+++ src/pages/404.js
@@ -8,7 +8,8 @@ import SEO from "../components/seo"
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <div className={'loading-spinner'}></div>
+    <h1>test</h1>
+    <span className={'loading-spinner'}></span>
   </Layout>
 )
 
