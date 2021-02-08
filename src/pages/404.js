fa1698ca1b1a2639bfc9c47db52931b56c7ce96c
Omer Gulen <omergulen@jotform.com>
2021-02-08 23:22:00 +0300

    404 :: test 2

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,15 +1,20 @@
 import React from "react"
 
+import Container from "../components/common/Container"
+import OuterContainer from "../components/common/OuterContainer"
+
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-
-
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
-    <h1>test</h1>
-    <span className={'loading-spinner'}></span>
+    <SEO title="shorten.club" />
+    <OuterContainer>
+      <Container>
+        <h1>shorten.club</h1>
+        <div className={'loading-spinner'}></div>
+      </Container>
+      </OuterContainer>
   </Layout>
 )
 
