195792705f0ad55836f36410c803dc082168ae3a
Omer Gulen <omergulen@jotform.com>
2021-02-13 17:03:47 +0300

    EditableNameHeaders :: init

---
+++ src/components/common/EditableNameHeader.js
@@ -0,0 +1,9 @@
+import styled from "@emotion/styled";
+import { EditText } from 'react-edit-text';
+
+const EditableNameHeader = styled(EditText)`
+  font-size: 2.5rem;
+  margin-bottom: 1rem;
+`;
+
+export default EditableNameHeader;
---
+++ src/components/common/EditableNameSubHeader.js
@@ -0,0 +1,9 @@
+import styled from "@emotion/styled";
+import { EditText } from 'react-edit-text';
+
+const EditableNameSubHeader = styled(EditText)`
+  font-size: 1rem;
+  margin-bottom: 1rem;
+`;
+
+export default EditableNameSubHeader;
